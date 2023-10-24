f4649d7adc95891e3ad7d0f929f91dfa6edcd3b7
KimSia, Sim <245021+simkimsia@users.noreply.github.com>
2023-10-24 10:00:16 +0800

    feat: add OneSentenceDescriptionField
    Signed-off-by: KimSia, Sim <245021+simkimsia@users.noreply.github.com>

--- src/django_anchor_modeling/fields.py
+++ src/django_anchor_modeling/fields.py
@@ -5,11 +5,24 @@ from django.db import models
 class BusinessIdentifierField(models.CharField):
     default_validators = [
         RegexValidator(
-            regex=r'^[1-9a-zA-Z_.-]+$',
-            message='Business ID must be alphanumeric, dots, hyphens, and underscores only.'
+            regex=r"^[1-9a-zA-Z_.-]+$",
+            message="Business ID must be alphanumeric, dots, hyphens, and underscores only.",
         )
     ]
 
     def __init__(self, *args, **kwargs):
-        kwargs.setdefault('max_length', 255)
+        kwargs.setdefault("max_length", 255)
         super().__init__(*args, **kwargs)
+
+
+class OneSentenceDescriptionField(models.TextField):
+    """
+    This field is used to store the description of a business event.
+    """
+
+    default_validators = [
+        RegexValidator(
+            regex=r"^[A-Za-z][^\.:\?!]*[\.:?!]$",
+            message="This field should contain only one sentence.",
+        )
+    ]
